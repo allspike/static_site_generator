946e7df5313488590e38d8e9ef02c16e33315f30
allspike <jspikerman@icloud.com>
2025-09-09 08:25:29 -0700

    Added basic html and css files. Added a shell script to run the python code. Created a basic python main function to create a dummy class and print out the value. Created a textnode.py file to contain the TextType enum and TextNode class.

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+def main():
+    dummy = TextNode("This is some anchor text", "link", f"https://www.boot.dev")
+    print(dummy)
+
+if __name__ == "__main__":
+    main()
